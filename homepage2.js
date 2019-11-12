896933cf975e7a42701586ad8b5b77d781928126
Shirogoma7261 <55296293+Shirogoma7261@users.noreply.github.com>
2019-11-12 17:58:03 +0900

    Update homepage2.js

--- homepage2.js
+++ homepage2.js
@@ -3,6 +3,8 @@ $(function() {
   $('.return').click(function() {
     window.location.href = "";
   });
-
+  $('.programming-btn').click(function() {
+    window.location.href = "https://shirogoma7261.github.io/shirogoma-homepage-programming/";
+  });
 
 });
